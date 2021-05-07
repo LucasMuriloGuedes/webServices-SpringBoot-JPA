d81f1ff88a2817811661285ff81957aa43e9615e
LucasMuriloGuedes <lucasmurilo.guedes@gmail.com>
2021-05-07 15:55:37 -0400

    Order item, many-to-many association with extra attributes

---
+++ src/main/java/br/lucasmurilo/springboot/projetoSpringBoot/entities/OrderItem.java
@@ -0,0 +1,71 @@
+package br.lucasmurilo.springboot.projetoSpringBoot.entities;
+
+import br.lucasmurilo.springboot.projetoSpringBoot.pk.OrdemItemPk;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    @EmbeddedId
+    private OrdemItemPk id;
+    private Integer quantity;
+    private double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItem orderItem = (OrderItem) o;
+
+        return id != null ? id.equals(orderItem.id) : orderItem.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
