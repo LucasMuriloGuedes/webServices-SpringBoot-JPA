a9e9d07ff6957d7f41b4212f597c07b8f18c2a1f
LucasMuriloGuedes <lucasmurilo.guedes@gmail.com>
2021-05-07 22:09:48 -0400

    Exception Handler

--- src/main/java/br/lucasmurilo/springboot/projetoSpringBoot/services/UserService.java
+++ src/main/java/br/lucasmurilo/springboot/projetoSpringBoot/services/UserService.java
@@ -4,6 +4,8 @@ import br.lucasmurilo.springboot.projetoSpringBoot.entities.User;
 import br.lucasmurilo.springboot.projetoSpringBoot.repositories.UserRepository;
 import br.lucasmurilo.springboot.projetoSpringBoot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +30,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        }catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch(DataIntegrityViolationException e){
+        }
+
     }
 
     public User update(Long id, User obj){
---
+++ src/main/java/br/lucasmurilo/springboot/projetoSpringBoot/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package br.lucasmurilo.springboot.projetoSpringBoot.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String msg){
+        super(msg);
+    }
+
+}
